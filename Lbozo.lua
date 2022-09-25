e173c33cad1fdef9a3a41fa8f0f2950b38a66cc6
omgitsperhapz <perhapzrblx@gmail.com>
2022-09-24 21:40:55 -0400

    f

--- Lbozo.lua
+++ Lbozo.lua
@@ -1199,7 +1199,7 @@ if game.PlaceId == 8304191830 then
         end
 
         if not Settings.Raid[MapName] or not Settings.Raid[MapName].Enabled then raid = false MapName = string.split(ChallengeInfo.current_level_id.Value,"_")[1] end
-        if not Settings.Challenges[MapName] or not Settings.Challenges[MapName].Enabled or LastChallenge == ChallengeInfo.current_challenge_uuid.Value or raid then challenge = false end
+        if not Settings.Challenges[MapName] or not Settings.Challenges[MapName].Enabled or LastChallenge == ChallengeInfo.current_challenge_uuid.Value or raid or (hasAMission() and Settings.DoMissions) then challenge = false end
 
         Lobby = FindOpenLobby(challenge, raid)
         task.wait()
@@ -1360,7 +1360,7 @@ if game.PlaceId == 8304191830 then
         
         print(MapName)
         if not (Settings.Raid or Settings.Raid[MapName] or Settings.Raid[MapName].Enabled) then raid = false MapName = string.split(ChallengeInfo.current_level_id.Value,"_")[1] end
-        if not Settings.DoChallenges or  not Settings.Challenges[MapName] or not Settings.Challenges[MapName].Enabled or LastChallenge == ChallengeInfo.current_challenge_uuid.Value or raid then  challenge = false end
+        if not Settings.DoChallenges or  not Settings.Challenges[MapName] or not Settings.Challenges[MapName].Enabled or LastChallenge == ChallengeInfo.current_challenge_uuid.Value or raid or (hasAMission() and Settings.DoMissions) then  challenge = false end
 
         print("Doing raid ".. tostring(raid))
         print("Doing Challenge ".. tostring(challenge))
